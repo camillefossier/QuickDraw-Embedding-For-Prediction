9131cacbad5e22b659aaefaad338beb6501825db
camillefossier <camille.fossier@gmail.com>
2020-02-24 16:08:51 +0100

    tests for signature's degrees

--- src/tests.py
+++ src/tests.py
@@ -57,6 +57,30 @@ def spline_knots(datasets, knots):
     tester.run()
     return tester
 
+def signature_degree(datasets, degrees):
+    K = len(datasets)
+    e1 = LogisticRegressorEvaluator()
+    e2 = SVMEvaluator()
+    #e3 = CNNEvaluator(input_shape=(10,10,1), num_classes=K)
+    e4 = SpectralClusteringEvaluator(K)
+    e5 = EMClusteringEvaluator(nb_clusters=K)
+    
+    signatures = [Signature(4, nb_to_keep=deg, embedding_id="signature") for deg in degrees] 
+    configs = generate_configs(
+        signatures,
+        [e1]
+    )
+    tester = Tester(
+        datasets,
+        configs,
+        store_data=True,
+        nb_lines=1000,
+        do_link_strokes=True,
+        do_rescale=True
+    )
+    tester.run()
+    return tester
+
 if __name__ == '__main__':
     datasets = [
         "../data/full_raw_axe.ndjson",
@@ -65,6 +89,10 @@ if __name__ == '__main__':
         "../data/full_raw_The Eiffel Tower.ndjson",
         "../data/full_raw_basketball.ndjson"
     ]
-    graph = spline_knots(datasets, list(range(2,102,2)))
+
+    sig_graph = signature_degree(datasets, list(range(10,341,30)))
+    sig_graph.show_curves(list(range(10,341,30)))
+    graph = spline_knots(datasets, list(range(2,102,5)))
+    graph.show_curves(list(range(2,102,5)))
     spline = spline_array(datasets)
     print(spline.latex_results(3))
