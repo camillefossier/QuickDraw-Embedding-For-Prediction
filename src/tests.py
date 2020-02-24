65ccf639ac36340ec16a395536051745b340ca6e
camillefossier <camille.fossier@gmail.com>
2020-02-24 13:26:29 +0100

    score evolution depending on spline knots

--- src/tests.py
+++ src/tests.py
@@ -34,8 +34,28 @@ def spline_array(datasets):
     return tester
 
 def spline_knots(datasets, knots):
-    splines = [Spline(abscissa=Drawing.X, ordinate=Drawing.Y, nb_knots=knot)]
-    # TODO 
+    K = len(datasets)
+    e1 = LogisticRegressorEvaluator()
+    e2 = SVMEvaluator()
+    #e3 = CNNEvaluator(input_shape=(10,10,1), num_classes=K)
+    e4 = SpectralClusteringEvaluator(K)
+    e5 = EMClusteringEvaluator(nb_clusters=K)
+    
+    splines = [Spline(abscissa=Drawing.X, ordinate=Drawing.Y, nb_knots=knot) for knot in knots] 
+    configs = generate_configs(
+        splines,
+        [e1, e2, e4, e5]
+    )
+    tester = Tester(
+        datasets,
+        configs,
+        store_data=True,
+        nb_lines=1000,
+        do_link_strokes=True,
+        do_rescale=True
+    )
+    tester.run()
+    return tester
 
 if __name__ == '__main__':
     datasets = [
@@ -45,6 +65,6 @@ if __name__ == '__main__':
         "../data/full_raw_The Eiffel Tower.ndjson",
         "../data/full_raw_basketball.ndjson"
     ]
-        
+    graph = spline_knots(datasets, list(range(2,102,2)))
     spline = spline_array(datasets)
     print(spline.latex_results(3))
